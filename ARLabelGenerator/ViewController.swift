476a764a07b269934d2bd948c74b8ad50587b801
Brenden <Brendenyork15@gmail.com>
2021-12-03 12:23:33 -0500

    added comments to recent code.

--- ARLabelGenerator/ViewController.swift
+++ ARLabelGenerator/ViewController.swift
@@ -14,10 +14,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    /*******************************************
+     Download and add Resnet50 model to project.
+     Setup configuration and vision request handler.
+    ***********************************************************/
+    
     private var MLModel = try! Resnet50(configuration: MLModelConfiguration())
     private var hitTestResult :ARHitTestResult!
     private var VRH = [VNRequest]()
     
+    /*******************************************
+     Load the scene, show statistics for monitoring application
+    ***********************************************************/
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
@@ -27,28 +35,57 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         registerGestureRecognizers()
     }
     
+    /*******************************************
+     Register a Gesture recognizer. Picks up tapping on screen anywhere.
+    ***********************************************************/
+    
     private func registerGestureRecognizers() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(screenTap))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
+    /*******************************************
+     screenTap Will be called whenever user taps on screen.
+     Gets touch location, and centers it on object.
+    ***********************************************************/
     @objc func screenTap(recognizer :UIGestureRecognizer) {
         let sceneView = recognizer.view as! ARSCNView
         let tapObject = self.sceneView.center
     
+        /*******************************************
+        gets current Frame from session (whats in view of camera)
+        hitTest Result acts soemwhat like a plane, We get the touch location
+        Then check for feature points and return if empty if not hitTestResults is set to itself.
+        ***********************************************************/
         guard let currentFrame = sceneView.session.currentFrame else { return }
         let hitTestResults = sceneView.hitTest(tapObject, types: .featurePoint)
         if hitTestResults.isEmpty { return }
         guard let hitTestResult = hitTestResults.first else { return }
         self.hitTestResult = hitTestResult
+        /*******************************************
+         A Core Video pixel buffer is an image buffer that holds pixels in main memory.
+         Pixel buffer is required for the vision request.
+        ***********************************************************/
         let pixelBuffer = currentFrame.capturedImage
         performVisionRequest(pixelBuffer: pixelBuffer)
     }
+    /*******************************************
+    get a node
+    pass in text from prediction
+    hitTestResults contains everything related to position of where the user clicked.
+    use cordinates of where user clicked to place sphere and aid  in text placemnet above sphere anchor.
+    ***********************************************************/
     private func displayPredictions(text :String) {
         let node = createText(text: text)
         node.position = SCNVector3(self.hitTestResult.worldTransform.columns.3.x, self.hitTestResult.worldTransform.columns.3.y, self.hitTestResult.worldTransform.columns.3.z)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
-    
+    /*******************************************
+     takes in text and returns you a node.
+     creates anchor point for a sphere that is created an appears where the user clicks.
+     text appears above the achor sphere.
+     Sphere and text color added through firstMatireal.diffuse
+     scale and size of text, and anchor added for more practical appreance
+    ***********************************************************/
     private func createText(text: String) -> SCNNode {
         let parentNode = SCNNode()
         let anchor = SCNSphere(radius: 0.01)
@@ -69,7 +106,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         parentNode.addChildNode(textNode)
         return parentNode
     }
-    
+    /*******************************************
+     Create a vision model and request
+     pass in the model in which the request is going to run.
+     handle errors with a return if nil
+     creates observation of what coreML belives the item is, with a confidence rating
+    ***********************************************************/
     private func performVisionRequest(pixelBuffer :CVPixelBuffer) {
         let visionRequestModel = try! VNCoreMLModel(for: self.MLModel.model)
         let visionRequest = VNCoreMLRequest(model: visionRequestModel) { visionRequest, error in
@@ -81,6 +123,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 self.displayPredictions(text: observation.identifier)
             }
         }
+        /*******************************************
+        Takes the center of the current frame,
+        crops the image to contain the touched object
+        feeds cropped image to coreML  and attempts to identify the item.
+        populates vision reuqest array. This is an array of vision reuqest. (can have multiple)
+        ***********************************************************/
         visionRequest.imageCropAndScaleOption = .centerCrop
         self.VRH = [visionRequest]
         let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [:])
@@ -102,7 +150,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
 }
 
-// Helper function
+// Helper function for text alignmnet
 fileprivate func convertFromCATextLayerAlignmentMode(_ input: CATextLayerAlignmentMode) -> String {
     return input.rawValue
 }
